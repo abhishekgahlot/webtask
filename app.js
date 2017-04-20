e946004f940e6041795217f38be5e84805ea63bb
AbhishekGahlot <me@abhishek.it>
2017-04-21 03:07:07 +0530

    final change for webtask

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 'use latest';
 
 import express from 'express';
+import request from 'request';
+import bodyParser from 'body-parser'
 import { fromExpress } from 'webtask-tools';
 
 const app = express();
+app.use(bodyParser.urlencoded());
+
+// Get this from https://developers.facebook.com/tools/explorer/
+const facebookAccessToken = 'TOKEN_HERE';
+const appAuthToken = 'BATMAN'; //specify this as your app password
 
 const htmlTemplate = function(params) { 
   return `<!DOCTYPE html>
@@ -11,9 +18,13 @@ const htmlTemplate = function(params) {
     <head>
       <link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/switchery/0.8.2/switchery.min.css">
-      <style>.form-control:focus {border-color:#3ba0ff;} .form-group{margin-bottom:30px;} .panel-heading{ padding: 17px 25px; } .panel-body{padding-top:30px;} .btn-success, .btn-success:hover { background-color: #3ba0ff; border-color: #3ba0ff;} </style>
+      <style>.form-control:focus {border-color:#3ba0ff;} .form-group{margin-bottom:30px;} .panel-heading{ padding: 17px 25px; } .panel-body{padding-top:30px;} .btn-success, .btn-success:hover, .btn-success:focus { background-color: #3ba0ff; border-color: #3ba0ff;} </style>
     </head>
    <body style="padding:80px;background: #f1f1f1;">
+      <div class="panel panel-default" id="response-wrapper" style="display:none;">
+        <div class="panel-body" id="response" style="padding-top:15px;">
+        </div>
+      </div>
       <div class="panel panel-default" style="border-color:#bbbbbb;">
         <div class="panel-heading">
           <h3 class="panel-title" style="font-size: 20px;">Publish Post to Social Platforms</h3>
@@ -35,12 +46,6 @@ const htmlTemplate = function(params) {
             <div class="col-sm-11">
               <label for="minimal-input-red" style="width:100px">Facebook</label>
               <input id="facebook" type="checkbox" class="js-switch-blue" />
-              <br><br>
-              <label for="minimal-input-red" style="width:100px">Twitter</label>
-              <input id="twitter" type="checkbox" class="js-switch-blue" />
-              <br><br>
-              <label for="minimal-input-green" style="width:100px">Google Plus</label>
-              <input id="google" type="checkbox" class="js-switch-blue" />
               <br><br>
             </div>
           </div>
@@ -67,46 +72,60 @@ const htmlTemplate = function(params) {
         var switchery = new Switchery(html, {color: '#3ba0ff'});
     });
     function getForm() {
-      var auth = document.getElementById('auth');
+      var auth = document.getElementById('auth').value;
       var post = document.getElementById('post').value;
       var facebook = document.getElementById('facebook').checked;
-      var twitter = document.getElementById('twitter').checked;
-      var google = document.getElementById('google').checked;
-      return [post, [facebook, twitter, google], auth];
+      return [post, [facebook], auth];
     }
 
-    document.getElementById('submit').onclick = function(e) {
+    $('#submit').on("click", function(e) {
       e.preventDefault();
-      console.log(getForm());
-    }
+      var data = getForm();
+      $.post(window.location.href + "/post", { post: data[0], facebook: data[1][0] })
+      .done(function(data) {
+        $("#response-wrapper").show()
+        $("#response").text(data);
+      });
+    });
    </script>
 </html>`;
 };
 
+function facebookPost(accessToken, post) {
+  const url = 'https://graph.facebook.com/v2.9/me/feed';
+  const qs = {
+    access_token: accessToken,
+    message: post
+  };
 
-function facebookPost() {
-
+  return new Promise((resolve, reject) => {
+    request.post({url, qs}, (err, resp, body) => {
+      if (err) { reject(err); return; }
+      resolve(JSON.parse(body));
+    });
+  });
 }
-
-function googlePost() {
-
-}
-
-function twitterPost() {
-
-}
-
-app.get('/:website', (req, res) => {
-  res.send(req.params.website);
-});
-
 
 app.get('/', (req, res) => {
   res.send(htmlTemplate());
 });
 
 app.post('/post', (req, res) => {
-  res.send(req.params)
+  let post = req.body.post;
+  let facebookBool = req.body.facebook;
+  let auth = req.body.auth;
+
+  if (facebookBool && auth === appAuthToken) {
+    facebookPost(facebookAccessToken, post)
+    .then((data) => {
+      res.send(JSON.stringify(data));
+    })
+    .catch((err) => {
+      res.send(JSON.stringify(err));
+    });
+  } else {
+    res.send('Your authorization code is incorrect.');
+  }
 });
 
 module.exports = fromExpress(app);
